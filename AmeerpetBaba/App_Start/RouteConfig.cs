a479503ccc4d0044dc2b4e8df7b869c030492f9f
chaitanyakmr <chaitanyakmr8@gmail.com>
2018-01-03 21:08:38 +0530

    Added the Institute Controller and Changed the default route

--- AmeerpetBaba/App_Start/RouteConfig.cs
+++ AmeerpetBaba/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace AmeerpetBaba
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Institute", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ AmeerpetBaba/Controllers/InstituteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AmeerpetBaba.Controllers
+{
+    public class InstituteController : Controller
+    {
+        // GET: Institute
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
